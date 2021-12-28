eb80ea359e490ca1894ed16bbc170f131acf8ac1
Kirill Ki <kirillkalapov@gmail.com>
2021-12-28 16:23:46 +0100

    Part 212

--- DiceeSwiftUI/ContentView.swift
+++ DiceeSwiftUI/ContentView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+          
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: 4)
+                        .padding()
+                    DiceView(n: 4)
+                        .padding()
+                }
+                .padding()
+                Spacer()
+                Button(action: {}) {
+                    Text("Roll")
+                        .font(.system(size: 44))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+                .cornerRadius(20)
+               // .shadow(radius: 7)
+                .shadow(color: .green, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 3, y: 3)
+                
+            }
+                
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
     }
 }
