89787224d193458a81c57fdfa2fd78d08a8e1e7d
Kirill Ki <kirillkalapov@gmail.com>
2021-12-28 14:58:50 +0100

    Initial Commit

---
+++ DiceeSwiftUI/DiceeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeSwiftUIApp.swift
+//  DiceeSwiftUI
+//
+//  Created by  Mr.Ki on 28.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct DiceeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
